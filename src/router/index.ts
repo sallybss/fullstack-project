39ea140b197ca036812edbd2be4a7e8b0cbec960
SirbuMadalina <madsir01@easv365.dk>
2026-02-22 16:56:25 +0100

    sign in sign up without header and footer

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,8 @@ import RecipeView from '../pages/RecipeView.vue'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/signin', component: SignIn },
-  { path: '/signup', component: SignUp },
+  { path: '/signin', component: SignIn, meta: { hideLayout: true }},
+  { path: '/signup', component: SignUp, meta: { hideLayout: true } },
   //(dynamic id, when user clicks recipe1 in the Url is /recipes/1)
   { path: '/recipes/:id', name: 'recipe', component: RecipeView },
 ]
